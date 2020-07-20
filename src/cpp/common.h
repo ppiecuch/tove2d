a881f5b1945bd2259c0532359c49192f6b807433
Pawel Piecuch <pawel.piecuch@roche.com>
2020-07-20 11:44:19 +0200

    cleanup dependencies

--- src/cpp/common.h
+++ src/cpp/common.h
@@ -44,8 +44,8 @@ BEGIN_TOVE_NAMESPACE
 #include "../thirdparty/nanosvg/src/nanosvgrast.h"
 END_TOVE_NAMESPACE
 
-#include "../thirdparty/clipper.hpp"
 #include "../thirdparty/polypartition/src/polypartition.h"
+#include "../thirdparty/clipper.hpp"
 
 #if _MSC_VER
 #define M_PI 3.1415926535897932384626433832795
@@ -63,11 +63,11 @@ inline void store_gpu_float(float &p, float x) {
 	p = x;
 }
 
-#if defined(TOVE_F16C) && defined(__clang__)
+#if defined(__clang__)  && defined(__F16C__)
 inline void store_gpu_float(uint16_t &p, float x) {
 	*reinterpret_cast<__fp16*>(&p) = x;
 }
-#elif defined(TOVE_F16C) && defined(__GNUC__)
+#elif defined(__GNUC__) && defined(__F16C__)
 #include <x86intrin.h>
 inline void store_gpu_float(uint16_t &p, float x) {
 	p = _cvtss_sh(x, 0);
