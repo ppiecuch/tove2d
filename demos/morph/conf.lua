32251b9accfe7be85b1bd6e20d7f78eb3c32f2c4
Pawel Piecuch <pawel.piecuch@roche.com>
2020-08-23 15:31:39 +0200

    cleanup/remove lua files - not necessery yet

--- demos/morph/conf.lua
+++
@@ -1,7 +0,0 @@
-function love.conf(t)
-	-- setting msaa is important to get antialiased
-	-- results for the mesh renderer. you should not
-	-- set this if you only use gpux renderers.
-
-	t.window.msaa = 2
-end
